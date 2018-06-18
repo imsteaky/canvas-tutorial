d28533d37d6f86c1153e4bac13892ff0e0d79b27
Nick Dambrosio <ndambro303@gmail.com>
2018-06-18 14:58:03 -0600

    Drawing multiple triangles

--- script.js
+++ script.js
@@ -4,13 +4,16 @@ function draw() {
     var ctx = canvas.getContext('2d');
 
     ctx.beginPath();
-    ctx.arc(75, 75, 50, 0, Math.PI * 2, true); //Outer Circle
-    ctx.moveTo(110, 75);
-    ctx.arc(75, 75, 35, 0, Math.PI, false); //Mouth clockwise
-    ctx.moveTo(65, 65);
-    ctx.arc(60, 65, 5, 0, Math.PI * 2, true); //Left Eye
-    ctx.moveTo(95, 65);
-    ctx.arc(90, 65, 5, 0, Math.PI * 2, true); //Right eye
+    ctx.moveTo(25, 25);
+    ctx.lineTo(105, 25);
+    ctx.lineTo(25, 105);
+    ctx.fill();
+
+    ctx.beginPath();
+    ctx.moveTo(125, 125);
+    ctx.lineTo(125, 45);
+    ctx.lineTo(45, 125);
+    ctx.closePath();
     ctx.stroke();
   }
 }
